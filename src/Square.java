f7241845069ad5a10ff8dc08ac546e4f58e99ed2
Joonoothoon <jaqunell@stu.naperville203.org>
2022-10-11 11:39:33 -0500

    works

--- src/Square.java
+++ src/Square.java
@@ -1,43 +1,27 @@
-
-/**
- * Represents a single square in the maze.
- *
- * @author gcschmit
- * @version 1 June 2017
- */
 public class Square
 {
-
     public static final int SPACE = 0;
     public static final int WALL = 1;
     public static final int START = 2;
     public static final int EXIT = 3;
     
+    private int type;
+    private int row;
+    private int col;
+
     /**
      * Constructor for objects of class Square
+     * 
+     * @param initialRow    the row for this Square in the Maze
+     * @param initialCol    the column for this Square in the Maze
+     * @param initialType   the type (space, wall, start, exit) for this
+     *                          Square in the Maze
      */
-    public Square( int row, int col, int type )
+    public Square(int initialRow, int initialCol, int initialType)
     {
-    }
-
-    /**
-     * Returns this square's row
-     *
-     * @return    this square's row
-     */
-    public int getRow()
-    {
-    	return 0;
-    }
-
-    /**
-     * Returns this square's column
-     *
-     * @return    this square's column
-     */
-    public int getCol()
-    {
-        return 0;
+        this.row = initialRow;
+        this.col = initialCol;
+        this.type = initialType;
     }
 
     /**
@@ -47,6 +31,87 @@ public class Square
      */
     public int getType()
     {
-        return 0;
+        return this.type;
+    }
+
+    /**
+     * Returns this square's row
+     *
+     * @return    this square's row
+     */
+    public int getRow()
+    {
+        return this.row;
+    }
+
+    /**
+     * Returns this square's column
+     *
+     * @return    this square's column
+     */
+    public int getCol()
+    {
+        return this.col;
+    }
+
+    @Override
+    public String toString()
+    {
+        String str = null;
+
+        switch(this.getType())
+        {
+            case Square.SPACE:
+            {
+                str = "_";
+                break;
+            }
+            case Square.WALL:
+            {
+                str = "#";
+                break;
+            }
+            case Square.START:
+            {
+                str = "S";
+                break;
+            }
+            case Square.EXIT:
+            {
+                str = "E";
+                break;
+            }
+        }
+
+        return str;
+    }
+
+    @Override
+    public boolean equals(Object other)
+    {
+        // self check
+        if(this == other)
+        {
+            return true;
+        }
+
+        // null check
+        if(other == null)
+        {
+            return false;
+        }
+
+        // type check
+        if(this.getClass() != other.getClass())
+        {
+            return false;
+        }
+
+        Square otherSq = (Square)other;
+
+        // field comparision
+        return (this.row == otherSq.row) &&
+                (this.col == otherSq.col) &&
+                (this.type == otherSq.type);
     }
 }
