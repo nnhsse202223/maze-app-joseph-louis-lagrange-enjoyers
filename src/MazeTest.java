8d4a3389dfd64d8858968f0f8580ed556e68c43f
MaxNguyen5040 <maxnguyen3012@gmail.com>
2022-10-13 11:50:48 -0500

    Worked on Mazetester

---
+++ src/MazeTest.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MazeTest {
+    public MazeTest(){
+
+    }
+
+    @Test
+    public void testNeighbors()
+    {
+
+    }
+
+    @Test
+    public void testCorners()
+    {
+
+    }
+
+    @Test
+    public void testBorders()
+    {
+
+    }
+    //Test There should also be a test to print out the maze, and to confirm your getStart and getFinish methods return the correct squares.
+    //look at other things!
+}
