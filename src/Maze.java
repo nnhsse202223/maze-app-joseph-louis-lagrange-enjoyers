db6c0072c21ff07a4713dbb711f1a79c53c658f8
MaxNguyen5040 <maxnguyen3012@gmail.com>
2022-10-13 11:37:04 -0500

    Implemented Maze Skeleton Class

---
+++ src/Maze.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Maze {
+    private Square[][]  maze;   // somewhere as a class variable
+    public Maze(){}
+
+
+    public boolean loadMaze(String fname){
+        return true;
+    }
+
+    public ArrayList<Square> getNeighbors(Square sq){
+        return null;
+    }
+
+    public Square getStart(){
+        return null;
+    }
+
+    public Square getFinish(){
+        return null;
+    }
+
+    public void reset(){
+
+    }
+
+    public String toString(){
+        return "";
+    }
+
+
+
+
+}
