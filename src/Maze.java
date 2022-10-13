ad0227c496ba30c5dab05c7a18048f1acbb7e93c
Joonoothoon <jaqunell@stu.naperville203.org>
2022-10-13 11:50:28 -0500

    Started maze code

--- src/Maze.java
+++ src/Maze.java
@@ -1,12 +1,30 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Maze {
     private Square[][]  maze;   // somewhere as a class variable
+    private Square start;
+    private Square finish;
+    private int numRows;
+    private int numCols;
+
     public Maze(){}
 
 
-    public boolean loadMaze(String fname){
-        return true;
+    public boolean loadMaze(String fname)
+        throws FileNotFoundException{
+        
+        Scanner s = new Scanner(new File(fname));
+        numRows = s.nextInt();
+        numCols = s.nextInt();
+        this.maze = new Square[numRows][numCols];
+        for (int row=0; row < numRows; row++) {
+            for (int col=0; col < numCols; col++) {
+                maze[row][col] = null;
+    }
+}
     }
 
     public ArrayList<Square> getNeighbors(Square sq){
