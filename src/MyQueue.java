890e005c7d5774039c55a15706b48ab513e316b6
Joonoothoon <jaqunell@stu.naperville203.org>
2022-10-14 11:26:23 -0500

    Started Queue class

---
+++ src/MyQueue.java
@@ -0,0 +1,42 @@
+import java.util.NoSuchElementException;
+
+public class MyQueue<T> implements QueueADT<T>
+{
+
+    @Override
+    public void enqueue(T item) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public T dequeue() throws NoSuchElementException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public T front() throws NoSuchElementException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
