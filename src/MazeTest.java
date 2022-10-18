d8b36fb9108923f769b8b72e5b292a46e717e60b
Joonoothoon <jaqunell@stu.naperville203.org>
2022-10-18 11:19:12 -0500

    Fixed some code

--- src/MazeTest.java
+++ src/MazeTest.java
@@ -1,9 +1,7 @@
 import static org.junit.Assert.*;
-import javax.print.attribute.standard.MediaSize;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import java.io.File;
 import java.util.ArrayList;
 
 public class MazeTest {
