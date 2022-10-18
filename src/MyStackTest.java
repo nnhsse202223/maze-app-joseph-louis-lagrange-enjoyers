91b4aa1d598f3950d3aed5d7e36caf95579f55b9
MaxNguyen5040 <maxnguyen3012@gmail.com>
2022-10-17 21:11:58 -0500

    Finished MyStack and MyStack test

--- src/MyStackTest.java
+++ src/MyStackTest.java
@@ -1,7 +1,105 @@
+import static org.junit.Assert.*;
+import javax.print.attribute.standard.MediaSize;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.File;
+import java.util.ArrayList;
+
+import java.util.Stack;
+
 public class MyStackTest {
-    
+    public MyStackTest(){
+
+    }
+
+    @Before
+    public void setUp()
+    {
+    }
+ 
+     @After
+    public void tearDown()
+    {
+    }
+
+    @Test
+    public void testClear()
+    {
+        Stack stack = new Stack();
+        myStack stack2 = new myStack<>();
+        for(int i =0; i < 10; i++){
+            stack.push(i);
+            stack2.push(i);
+        }
+        stack.clear();
+        stack2.clear();
+        assertEquals(stack.toString(),stack2.getList().toString().toString());
+    }
+
+    @Test
+    public void testPush()
+    {
+        Stack stack = new Stack();
+        myStack stack2 = new myStack<>();
+        for(int i =0; i < 10; i++){
+            stack.push(i);
+            stack2.push(i);
+        }
+        stack.push(0);
+        stack2.push(0);
+        assertEquals(stack.toString(),stack2.getList().toString());
+    }
+
+    @Test
+    public void testPop()
+    {
+        Stack stack = new Stack();
+        myStack stack2 = new myStack<>();
+        for(int i =0; i < 10; i++){
+            stack.push(i);
+            stack2.push(i);
+        }
+        assertEquals(stack.pop(),stack2.pop());
+    }
+
+    @Test
+    public void testTop()
+    {
+        Stack stack = new Stack();
+        myStack stack2 = new myStack<>();
+        for(int i =0; i < 10; i++){
+            stack.push(i);
+            stack2.push(i);
+        }
+
+        assertEquals(stack.peek(),stack2.top());
+    }
 
 
+    @Test
+    public void testisEmpty()
+    {
+        Stack stack = new Stack();
+        myStack stack2 = new myStack<>();
+        for(int i =0; i < 10; i++){
+            stack.push(i);
+            stack2.push(i);
+        }
+        
+        assertEquals(stack.isEmpty(),stack2.isEmpty());
+    }
 
-    
+    @Test
+    public void testsize()
+    {
+        Stack stack = new Stack();
+        myStack stack2 = new myStack<>();
+        for(int i =0; i < 10; i++){
+            stack.push(i);
+            stack2.push(i);
+        }
+
+        assertEquals(stack.size(),stack2.size());
+    }
 }
--- src/myStack.java
+++ src/myStack.java
@@ -1,12 +1,67 @@
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.Stack;
+
+import javax.lang.model.element.Element;
 
 public class myStack<T> implements StackADT<T>{
     
+    private ArrayList<T> list;
     
     public myStack(){
-
+        this.list = new ArrayList<>();
     }
 
+    @Override
+    public boolean isEmpty(){
+        return list == null;
+    }
 
+    @Override
+    public void clear(){
+        while(list.size()>0){
+            list.remove(0);
+        }
+    }
+    
+    @Override
+    public void push(T item) {
+        list.add(item);
+    
+    }
+
+    public ArrayList getList(){
+        return this.list;
+    }
+
+    @Override
+    public T pop() throws NoSuchElementException {
+        if(this.isEmpty()){
+            throw new NoSuchElementException();
+        }
+        T last = list.get(list.size()-1); //last element
+        list.remove(last);
+        return last;
+    }
+
+    @Override
+    public T top() throws NoSuchElementException {
+        if(this.isEmpty()){
+            throw new NoSuchElementException();
+        }
+        return list.get(list.size()-1);
+    }
+
+    @Override
+    public int size() {
+        int count = 0;
+        for(T element : list){
+            count ++;
+        }
+        return count;
+    }
 
 }
+
+
+
