2ca3470def6c2278812a1ad88b2fa0fd71709e73
Joonoothoon <jaqunell@stu.naperville203.org>
2022-10-21 11:22:57 -0500

    Created Maze Solver

---
+++ src/MazeSolverQueue.java
@@ -0,0 +1,32 @@
+public class MazeSolverQueue extends MazeSolver
+{
+
+    public MazeSolverQueue(Maze maze) 
+    {
+        super(maze);
+    }
+
+    @Override
+    public void makeEmpty() 
+    {
+        
+    }
+
+    @Override
+    public boolean isEmpty() 
+    {
+        return false;
+    }
+
+    @Override
+    public void add(Square sq) 
+    {
+        
+    }
+
+    @Override
+    public Square next() 
+    {
+        return null;
+    }
+}
